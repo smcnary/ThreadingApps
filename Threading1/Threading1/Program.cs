5016f33942513488372eb944f11467345dac7eac
Sean McNary <sean.mcnary@aaon.com>
2019-10-10 12:51:48 -0500

    non-safe thread

---
+++ Threading1/Threading1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading1
+{
+	class ThreadTest
+	{
+		private static bool _done;
+
+		static void Main(string[] args)
+		{
+			//Separate thread
+			new Thread(Go).Start();
+			//Main thread;
+			Go();
+		}
+
+		static void Go()
+		{
+			if (!_done)
+			{
+				_done = true;
+				Console.WriteLine("Done");
+
+				
+
+				Console.ReadLine();
+			}
+		}
+	}
+}
